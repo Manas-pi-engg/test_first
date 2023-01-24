30a027f14cf7771969d24de1426496d31abef46d
Manas-pi-engg <122532804+Manas-pi-engg@users.noreply.github.com>
2023-01-24 16:48:03 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,15 +1,10 @@
-public class Sum 
-{
+class Sum{
     public static void main(String args[])
-    {
-        int a=5,b=7;
+    {// comment
+        int a = 5, b = 7;
 
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println(a+b);
-      
     }
-  
 }
-
-
