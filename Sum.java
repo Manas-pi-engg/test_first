58e594817251902619e17bd641052742c27c8430
Manas-pi-engg <122532804+Manas-pi-engg@users.noreply.github.com>
2023-01-24 16:35:45 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,15 @@
+public class Sum 
+{
+    public static void main(String args[])
+    {
+        int a=5,b=7;
+
+        System.out.println("hello world");
+        System.out.println("hello world2");
+        System.out.println(a+b);
+      
+    }
+  
+}
+
+
